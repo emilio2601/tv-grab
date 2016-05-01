6a048533ea8c67c88dd93569d2f1ef04e3313694
Emilio Mendoza <emilio2601@gmail.com>
2016-04-30 19:55:09 -0500

    Removed warning message

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import sys
 app = Flask(__name__)
 CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 @app.errorhandler(404)
